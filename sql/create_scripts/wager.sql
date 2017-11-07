326ac7079be6146fd979138d41a3320f7921f61b
Kerry Sim <kerrysim0727@gmail.com>
2017-11-06 22:41:06 -0600

    Update wager.sql

--- sql/create_scripts/wager.sql
+++ sql/create_scripts/wager.sql
@@ -1,5 +1,7 @@
 CREATE TABLE wager (
-	contestant_id INT(5) UNSIGNED NOT NULL FOREIGN KEY REFEReNCES Contestant(contestant_id),
-  	question_id INT(5) UNSIGNED NOT NULL FOREIGN KEY REFERENCES Question(question_id), 
-  	wager_amount INT,
+	contestant_id INT(5) UNSIGNED NOT NULL, 
+	FOREIGN KEY (contestant_id) REFERENCES contestant(contestant_id),
+  	question_id INT(5) UNSIGNED NOT NULL, 
+	FOREIGN KEY (question_id) REFERENCES question(question_id),  	
+	wager_amount INT,
 	);
