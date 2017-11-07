1fc15b5bb1ba828be6def459400e55fa29e9544a
Kerry Sim <kerrysim0727@gmail.com>
2017-11-06 22:44:38 -0600

    Update wager.sql

--- sql/create_scripts/wager.sql
+++ sql/create_scripts/wager.sql
@@ -1,7 +1,7 @@
 CREATE TABLE wager (
 	contestant_id INT(5) UNSIGNED NOT NULL, 
-	FOREIGN KEY (contestant_id) REFERENCES contestant(contestant_id) ON DELETE CASCADE, 
+	FOREIGN KEY (contestant_id) REFERENCES contestant(contestant_id) ON DELETE CASCADE 
   	question_id INT(5) UNSIGNED NOT NULL, 
-	FOREIGN KEY (question_id) REFERENCES question(question_id) ON DELETE CASCADE,  	
-	wager_amount INT,
+	FOREIGN KEY (question_id) REFERENCES question(question_id) ON DELETE CASCADE  	
+	wager_amount INT(10),
 	);
